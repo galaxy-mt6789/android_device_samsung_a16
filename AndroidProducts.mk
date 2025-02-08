ab2a2b89cbd3c1a9c4fc06419830983ab7d5755c
SavedByLight <mrfluffyoven@gmail.com>
2025-02-08 11:14:40 +0000

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a16.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a16-user \
+    twrp_a16-userdebug \
+    twrp_a16-eng
